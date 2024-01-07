5855b65feeb4db8c08f1c7f85c9209cb661c9d59
leonardoman9 <46076388+leonardoman9@users.noreply.github.com>
2024-01-07 18:13:13 +0100

    added things

--- scripts.py
+++
@@ -1,5 +0,0 @@
-
-
-def showFlag(screen, flags ,current_country, width, flag_size, height):
-        screen.fill((0, 0, 0))  # Set background color to black
-        screen.blit(flags[current_country], (width // 2 - flag_size[0] // 2, height // 2 - flag_size[1] // 2))
