788d158bb32b8dc0c5fcc12e46122689b5411c97
leonardoman9 <46076388+leonardoman9@users.noreply.github.com>
2024-01-07 16:41:24 +0100

    refactored the codebase

---
+++ gui.py
@@ -0,0 +1,133 @@
+import pygame
+import os
+from countries import countries
+import db
+
+class gui:
+    def __init__(self):
+        self.width, self.height = 800, 600
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.flag_size = (300, 200)
+        self.flags = {country: pygame.transform.scale(pygame.image.load(os.path.join("data/flags", filename)),
+                                                    self.flag_size) for country,
+                                                    filename in countries.items()}
+        self.font = pygame.font.Font("data/fonts/americancaptain.ttf", 30)
+        # Vertical separation between text elements
+        self.text_vertical_spacing = 20
+        # Text input variables
+        self.input_text = ""
+        self.input_border = pygame.Rect(252, self.height - 48, 196, 28)
+        self.input_rect = pygame.Rect(250, self.height - 50, 200, 32)
+        self.input_color = pygame.Color('#FFD700')  # Gold color for input box
+        self.input_active = False# Text input variables
+        self.input_text = ""
+        self.input_border = pygame.Rect(252, self.height - 48, 196, 28)
+        self.input_rect = pygame.Rect(250, self.height - 50, 200, 32)
+        self.input_color = pygame.Color('#FFD700')  # Gold color for input box
+        self.input_active = False
+        self.mouse_x, self.mouse_y = pygame.mouse.get_pos()
+        self.quit_button_rect = pygame.Rect(self.width // 2 - 100, self.height // 2 + 50, 100, 50)
+        self.rankings_button_rect = pygame.Rect(self.width // 2 + 20, self.height // 2 + 50, 140, 50)
+
+    def showGameOver(self, score, wrong_countries):
+        self.screen.fill((0, 0, 0))  # Black background
+        game_over_text = self.font.render(f"Game Over - Score: {score}", True, (255, 0, 0))
+        mistakes_text = self.font.render(f"Mistakes: {', '.join(wrong_countries)}", True, (255, 0, 0))
+
+        self.screen.blit(game_over_text, (self.width // 2 - game_over_text.get_width() // 2, self.height // 2 - game_over_text.get_height() // 2))
+
+        pygame.draw.rect(self.screen, (0, 255, 0), self.quit_button_rect)
+        quit_text = self.font.render("Quit", True, (0, 0, 0))
+        self.screen.blit(quit_text, (self.quit_button_rect.centerx - quit_text.get_width() // 2, self.quit_button_rect.centery - quit_text.get_height() // 2))
+
+        pygame.draw.rect(self.screen, (0, 0, 255), self.rankings_button_rect)  # Corrected this line
+        rankings_text = self.font.render("See Rankings", True, (0, 0, 0))
+        self.screen.blit(rankings_text, (self.rankings_button_rect.centerx - rankings_text.get_width() // 2, self.rankings_button_rect.centery - rankings_text.get_height() // 2))
+
+        pygame.display.flip()
+
+        pygame.time.delay(50)  # Add a delay to avoid flickering
+
+    def showRankings(self, db_filename):
+        self.screen.fill((0, 0, 0))  # Black background
+        rankings_text = self.font.render("Top 10 Rankings:", True, (255, 255, 255))  # White color for rankings text
+        self.screen.blit(rankings_text, (self.width // 2 - rankings_text.get_width() // 2, 50))
+
+        # Fetch and display top 10 rankings from the database (ordered by score)
+        top_scores = db.get_top_scores(db_filename,limit=10)
+
+        for i, (rank, timestamp, game_sequence, mistakes) in enumerate(top_scores):
+            rank_text = self.font.render(f"{i + 1}. Score: {rank} - {timestamp} - {mistakes}", True, (255, 255, 255))
+            self.screen.blit(rank_text, (self.width // 2 - rank_text.get_width() // 2, 100 + i * 30))
+
+        back_button_rect = pygame.Rect(self.width // 2 - 70, self.height - 50, 140, 50)
+        pygame.draw.rect(self.screen, (255, 0, 0), back_button_rect)
+        back_text = self.font.render("Back", True, (0, 0, 0))
+        self.screen.blit(back_text, (back_button_rect.centerx - back_text.get_width() // 2, back_button_rect.centery - back_text.get_height() // 2))
+
+        pygame.display.flip()
+
+        pygame.time.delay(50)  # Add a delay to avoid flickering
+
+    def getWidthHeight(self):
+        return self.width, self.height
+
+    def getScreen(self):
+        return self.screen
+
+    def get_height(self):
+        return self.height
+
+    def get_width(self):
+        return self.width
+
+    def getFlags(self):
+        return self.flags
+
+    def getFlagSize(self):
+        return self.flag_size
+
+    def getFont(self):
+        return self.font
+    def get_text_vertical_spacing(self):
+        return self.text_vertical_spacing
+
+    def get_input_text(self):
+        return self.input_text
+    def get_input_border(self):
+        return self.input_border 
+    def get_input_rect(self):
+        return self.input_rect 
+    def get_input_color(self):
+        return self.input_color 
+    def get_input_active(self):
+        return self.input_active 
+    def get_input_text(self):
+        return self.input_text
+    def get_input_borde(self):
+        return self.input_border 
+    def get_input_rect(self):    
+        return self.input_rect 
+    def get_input_color(self):    
+        return self.input_color 
+    def get_input_active(self):    
+        return self.input_active
+    def set_input_text(self, new_text):
+        self.input_text = new_text
+    def get_x_y(self):
+        return self.mouse_x, self.mouse_y
+    def get_mouse_x(self):
+        return self.mouse_x
+    def get_mouse_y(self):
+        return self.mouse_y
+
+    def set_mouse_x_y(self, mouse_pos):
+        print(self.mouse_x, ", ", self.mouse_y)
+        self.mouse_x, self.mouse_y = mouse_pos
+        print(self.mouse_x, ", ", self.mouse_y)
+        return self.mouse_x, self.mouse_y  # Add this line
+    def get_quit_button_rect(self):
+        return self.quit_button_rect
+    def get_rankings_button_rect(self):    
+        return self.rankings_button_rect
+
--- main.py
+++ main.py
@@ -5,81 +5,41 @@ import os
 import sqlite3
 from datetime import datetime
 from countries import countries
+import scripts
+import audio
+import db
+import gui
 
 # Initialize Pygame
 icon = pygame.image.load("data/icon.ico")
 pygame.display.set_icon(icon)
 pygame.init()
-# Set up display
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("FlagGuessr")
-# Load and resize flag images
-flag_size = (300, 200)
-flags = {country: pygame.transform.scale(pygame.image.load(os.path.join("data/flags", filename)), flag_size) for country, filename in countries.items()}
-
-# Font setup    
-font = pygame.font.Font(None, 36)
-
+g = gui.gui()
 # Database setup
 db_filename = "scores.db"
 
-def create_scores_table():
-    connection = sqlite3.connect(db_filename)
-    cursor = connection.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS scores
-                      (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       score INTEGER,
-                       timestamp TEXT)''')
-    connection.commit()
-    connection.close()
-
-def insert_score(score):
-    connection = sqlite3.connect(db_filename)
-    cursor = connection.cursor()
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    cursor.execute("INSERT INTO scores (score, timestamp) VALUES (?, ?)", (score, timestamp))
-    connection.commit()
-    connection.close()
-
-def get_top_scores(limit=10):
-    connection = sqlite3.connect(db_filename)
-    cursor = connection.cursor()
-    cursor.execute("SELECT score, timestamp FROM scores ORDER BY score DESC LIMIT ?", (limit,))
-    top_scores = cursor.fetchall()
-    connection.close()
-    return top_scores
-
 # Ensure the 'scores' table is created
-create_scores_table()
-
+db.create_scores_table(db_filename)
 # Load and play a random song from the "data" folder
-song_files = [f for f in os.listdir("data/music") if f.endswith(".mp3")]
-if song_files:
-    random_song = os.path.join("data/music", random.choice(song_files))
-    pygame.mixer.music.load(random_song)
-    pygame.mixer.music.play(-1)  # -1 makes the music loop indefinitely
+audio.playMusic()
+#Game constants
+MAXLIVES = 3
+INITSCORE = 0
 
 
 # Game variables
-current_country = random.choice(list(countries.keys())); print(f"CURRENT GAME SEQUENCE: {current_country}", end = ", ", flush=True)
-score = 0
-lives = 3
-
-# Text input variables
-input_text = ""
-input_rect = pygame.Rect(250, height - 50, 200, 32)
-input_color = pygame.Color('dodgerblue2')
-input_active = False
-
-# Vertical separation between text elements
-text_vertical_spacing = 20
+current_country = random.choice(list(countries.keys()))
+countries_list = []
+wrong_countries = []
+print(f"CURRENT GAME SEQUENCE: {current_country}", end=", ", flush=True)
+countries_list.append(current_country)
+score = INITSCORE
+lives = MAXLIVES
 
 # Game loop
 running = True
 game_over = False
 show_rankings = False
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -91,42 +51,45 @@ while running:
 
             elif event.key == pygame.K_RETURN:
                 # Check the user's input
-                if input_text.lower() == current_country.lower():
+                if g.get_input_text().lower() == current_country.lower():
                     # Correct guess
                     score += 1
                     current_country = random.choice(list(countries.keys()))
-                    print(current_country, end=", ", flush=True)  
-                    input_text = ""
+                    print(current_country, end=", ", flush=True)
+                    countries_list.append(current_country)
+                    g.set_input_text("")
                 else:
                     # Incorrect guess
                     lives -= 1
+                    wrong_countries.append(current_country)
+                    print(f"<-WRONG!", end=", ", flush = True)
                     if lives == 0:
                         # Game over if no lives left
-                        insert_score(score)  # Save the score to the database
+                        db.insert_score(db_filename, score, countries_list, wrong_countries)  # Save the score to the database
+                        print(f"\n\nFull game: {countries_list}")
+                        print(f"\nMistaken countries: {wrong_countries}")
                         game_over = True
 
             elif event.key == pygame.K_BACKSPACE:
-                input_text = input_text[:-1]
-
+                g.set_input_text(g.get_input_text()[:-1])
             else:
-                input_text += event.unicode
+                g.set_input_text(g.get_input_text() + event.unicode)
 
     # Display the current flag
-    screen.fill((128, 128, 128))  # Set background color to white
-    screen.blit(flags[current_country], (width // 2 - flag_size[0] // 2, height // 2 - flag_size[1] // 2))
-
+    scripts.showFlag(g.getScreen(), g.getFlags(), current_country, g.get_width(), g.getFlagSize(), g.get_height())
     # Display score and lives with increased vertical separation
-    score_text = font.render(f"Score: {score}", True, (0, 0, 0))
-    lives_text = font.render(f"Lives: {lives}", True, (0, 0, 0))
-    screen.blit(score_text, (10, 10))
-    screen.blit(lives_text, (width - 120, 10 + text_vertical_spacing))
+    score_text = g.getFont().render(f"Score: {score}", True, (255, 255, 255))  # White color for score
+    lives_text = g.getFont().render(f"Lives: {lives}", True, (255, 255, 255))
+    g.getScreen().blit(score_text, (10, 10))
+    g.getScreen().blit(lives_text, (g.get_width() - 120, 10 + g.get_text_vertical_spacing()))
 
     # Draw the input box
-    pygame.draw.rect(screen, input_color, input_rect, 2)
-    text_surface = font.render(input_text, True, (0, 0, 0))
+    pygame.draw.rect(g.getScreen(), g.get_input_color(), g.get_input_rect(), 2)
+    pygame.draw.rect(g.getScreen(), (0, 0, 0), g.get_input_border(), 2)  # Black color for input box border
+    text_surface = g.getFont().render(g.get_input_text(), True, (255, 255, 255), (0, 0, 0))  # White color for text, black background
     input_box_width = max(200, text_surface.get_width() + 10)
-    input_rect.w = input_box_width
-    screen.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
+    g.get_input_rect().w = input_box_width
+    g.getScreen().blit(text_surface, (g.get_input_rect().x + 5, g.get_input_rect().y + 5))
 
     pygame.display.flip()
 
@@ -137,37 +100,17 @@ while running:
                 if event.type == pygame.QUIT:
                     running = False
                     wait = False
-
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    mouse_x, mouse_y = pygame.mouse.get_pos()
-                    quit_button_rect = pygame.Rect(width // 2 - 100, height // 2 + 50, 100, 50)
-                    rankings_button_rect = pygame.Rect(width // 2 + 20, height // 2 + 50, 140, 50)
-
-                    if quit_button_rect.collidepoint(mouse_x, mouse_y):
+                    g.set_mouse_x_y(pygame.mouse.get_pos())
+                    if g.get_quit_button_rect().collidepoint(g.get_x_y()):
                         running = False
                         wait = False
-                    elif rankings_button_rect.collidepoint(mouse_x, mouse_y):
+                    elif g.get_rankings_button_rect().collidepoint(g.get_x_y()):
                         show_rankings = True
                         wait = False
-
-            # Display game over screen with Quit and Rankings buttons
-            screen.fill((255, 255, 255))
-            game_over_text = font.render(f"Game Over - Score: {score}", True, (255, 0, 0))
-            screen.blit(game_over_text, (width // 2 - game_over_text.get_width() // 2, height // 2 - game_over_text.get_height() // 2))
-
-            quit_button_rect = pygame.Rect(width // 2 - 150  , height // 2 + 50, 100, 50)
-            pygame.draw.rect(screen, (0, 255, 0), quit_button_rect)
-            quit_text = font.render("Quit", True, (255, 255, 255), (0,128,0))
-            screen.blit(quit_text, (quit_button_rect.centerx - quit_text.get_width() // 2, quit_button_rect.centery - quit_text.get_height() // 2))
-
-            rankings_button_rect = pygame.Rect(width // 2 + 50, height // 2 + 50, 140, 50)
-            pygame.draw.rect(screen, (0, 0, 255), rankings_button_rect)
-            rankings_text = font.render("See Rankings", True, (255, 255, 255), (0,128,0))
-            screen.blit(rankings_text, (rankings_button_rect.centerx - rankings_text.get_width() // 2, rankings_button_rect.centery - rankings_text.get_height() // 2))
-
-            pygame.display.flip()
-
-            pygame.time.delay(50)  # Add a delay to avoid flickering
+            # Display game over screen with Quit and Rankings buttons 
+            g.showGameOver(score,wrong_countries)
+            
 
     # Display rankings screen
     if show_rankings:
@@ -178,35 +121,18 @@ while running:
                     show_rankings = False
 
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    mouse_x, mouse_y = pygame.mouse.get_pos()
-                    quit_button_rect = pygame.Rect(width // 2 - 100, height // 2 + 50, 100, 50)
-                    back_button_rect = pygame.Rect(width // 2 - 70, height - 50, 140, 50)
+                    g.set_mouse_x_y(pygame.mouse.get_pos())
 
-                    if quit_button_rect.collidepoint(mouse_x, mouse_y):
+                    quit_button_rect = pygame.Rect(g.get_width() // 2 - 100, g.get_height() // 2 + 50, 100, 50)
+                    back_button_rect = pygame.Rect(g.get_width() // 2 - 70, g.get_height() - 50, 140, 50)
+
+                    if quit_button_rect.collidepoint(g.get_mouse_x(), g.get_mouse_y()):
                         show_rankings = False
-                    elif back_button_rect.collidepoint(mouse_x, mouse_y):
+                    elif back_button_rect.collidepoint(g.get_mouse_x(), g.get_mouse_y()):
                         show_rankings = False
 
             # Display rankings
-            screen.fill((255, 255, 255))
-            rankings_text = font.render("Top 10 Rankings:", True, (0, 0, 0))
-            screen.blit(rankings_text, (width // 2 - rankings_text.get_width() // 2, 50))
-
-            # Fetch and display top 10 rankings from the database (ordered by score)
-            top_scores = get_top_scores(limit=10)
-
-            for i, (rank, timestamp) in enumerate(top_scores):
-                rank_text = font.render(f"{i + 1}. Score: {rank} - {timestamp}", True, (0, 0, 0))
-                screen.blit(rank_text, (width // 2 - rank_text.get_width() // 2, 100 + i * 30))
-
-            back_button_rect = pygame.Rect(width // 2 - 70, height - 50, 140, 50)
-            pygame.draw.rect(screen, (255, 0, 0), back_button_rect)
-            back_text = font.render("Back", True, (255, 255, 255))
-            screen.blit(back_text, (back_button_rect.centerx - back_text.get_width() // 2, back_button_rect.centery - back_text.get_height() // 2))
-
-            pygame.display.flip()
-
-            pygame.time.delay(50)  # Add a delay to avoid flickering
+            g.showRankings(db_filename)
 
 # Quit Pygame
 pygame.quit()
